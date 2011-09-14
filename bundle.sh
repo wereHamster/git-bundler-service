3ea16c269e701f807b794d080d5f6ff954e162cc
Tomas Carnecky <tomas.carnecky@gmail.com>
2011-09-14 17:39:18 +0200

    Destroy repo after creating the bundle

--- bundle.sh
+++ bundle.sh
@@ -2,8 +2,8 @@
 
 ROOT="$1/data/bundles/$2"
 
-mkdir -p "$ROOT/repo"
-cd "$ROOT/repo"
+GIT_DIR="$ROOT/repo"; export GIT_DIR
+mkdir -p "$GIT_DIR"
 
 git init --bare
 git remote add origin --mirror "$3"
@@ -11,3 +11,4 @@ git remote add origin --mirror "$3"
 git fetch origin; git remote prune origin
 git bundle create "$ROOT/bundle" --all
 
+rm -rf $GIT_DIR
