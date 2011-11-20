0b97967b18ace959a0387df41475c2d93c2c3b5b
Tomas Carnecky <tomas.carnecky@gmail.com>
2011-11-20 22:46:39 +0100

    Run npm install as part of the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-git remote update
-git reset --hard $1
+git remote update && git reset --hard $1
+npm install --mongodb:native
 
 ./node_modules/.bin/coffee -c index.coffee app.coffee
 ./node_modules/.bin/stylus -c public/style.styl
