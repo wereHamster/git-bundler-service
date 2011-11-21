1447799e30aef23991d2f013f671e589998767b5
Tomas Carnecky <tomas.carnecky@gmail.com>
2011-11-21 03:22:10 +0100

    Use /dev/null as stdin for the coffee and stylus scripts

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ set -e
 git fetch origin && git reset --hard $1
 npm install --mongodb:native
 
-./node_modules/.bin/coffee -c index.coffee app.coffee
-./node_modules/.bin/stylus -c public/style.styl
+./node_modules/.bin/coffee -c index.coffee app.coffee </dev/null
+./node_modules/.bin/stylus -c public/style.styl </dev/null
